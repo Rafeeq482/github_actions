b28258e3c672abdf2ff178a973d007e2a219677a
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-02 11:49:01 +0530

    This is the new commit333

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "instance_ids" {
-  value = { for k, inst in module.ec2_instance : k => inst.id }
+output "instance_id" {
+  value = module.ec2_instance.id
 }
 
-output "public_ips" {
-  value = { for k, inst in module.ec2_instance : k => inst.public_ip }
+output "public_ip" {
+  value = module.ec2_instance.public_ip
 }
